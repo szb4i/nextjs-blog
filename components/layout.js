1087314272cf3e7fed9e4d3763123a74d139e2ce
szbai <szabolcs.nagy.hu@gmail.com>
2022-11-11 12:19:15 +0100

    name changed on dev branch

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Vercel Test";
+const name = "Vercel Test with PR";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
